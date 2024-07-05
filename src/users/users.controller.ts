e04fcc412c32420067cf64f207a1afabe6a1759e
Haruki <aratashiharuki@gmail.com>
2024-07-05 23:12:15 +0300

    Add debug version of createUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post('/create')
+    async createUser(@Body() createUserDto: CreateUserDto): Promise<void> {
+        await this.usersService.createUser(createUserDto)
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel('User') private userModel: Model<User>) {}
+
+    async createUser(userDto: CreateUserDto): Promise<void> {
+        await new this.userModel(userDto).save()
+    }
+}
