3eef2a8e0f2fbb4884efef4842f06b0e43780bbc
Haruki <aratashiharuki@gmail.com>
2024-07-06 03:58:50 +0300

    Add users base CRUD

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+    constructor(@InjectModel('User') private userModel: Model<User>) {}
+
+    async createUser(userDto: CreateUserDto): Promise<void> {
+        const password: string = await hash(userDto.password, 10)
+        userDto.password = password
+
+        await new this.userModel(userDto).save()
+    }
+
+    async findByUsername(username: string) {
+        const user = await this.userModel.findOne({username: username}).exec()
+
+        return user
+    }
+}
